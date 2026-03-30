a9994f912bf61f5f67486223cc9ef25d7372e247
Somya Devda <devsomya@gmail.com>
2026-03-30 12:06:50 +0530

    message

--- app.js
+++ app.js
@@ -1,12 +1,12 @@
-const express = require("express");
+const express = require('express');
 const app = express();
 
 const PORT = process.env.PORT || 10000;
 
-app.get("/", (req, res) => {
-  res.send("Hello World");
+app.get('/', (req, res) => {
+  res.send('Server is running');
 });
 
-app.listen(PORT, "0.0.0.0", () => {
+app.listen(PORT, '0.0.0.0', () => {
   console.log(`Server running on port ${PORT}`);
 });
